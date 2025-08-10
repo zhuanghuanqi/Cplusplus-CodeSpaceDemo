b0dac36a3cde9c60e4f6e3a933fe504f891e19a0
zhanghuaiqiang <118420787+zhuanghuanqi@users.noreply.github.com>
2025-08-10 00:17:38 +0000

    修改文字

--- main.cpp
+++ main.cpp
@@ -138,7 +138,7 @@ public:
         std::cout << "*****   3、删除联系人  *****" << std::endl;
         std::cout << "*****   4、查找联系人  *****" << std::endl;
         std::cout << "*****   5、修改联系人  *****" << std::endl;
-        std::cout << "*****   6、删除联系人  *****" << std::endl;
+        std::cout << "*****   6、清空联系人  *****" << std::endl;
         std::cout << "*****   0、退出通讯录  *****" << std::endl;
         std::cout << "***************************" << std::endl;
     }
